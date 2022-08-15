5f67e6bbc49686bb1be33e3d1f0a05467be4ebfd
FABRICIO MARTINS DA SILVA <fabriciomdasilva8@gmail.com>
2022-08-15 18:47:00 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -2,6 +2,12 @@
   include_once("templates/header.php");
 ?>
   <h1>Página de contato</h1>
+  <div align="center" > 
+  <a href="https://api.whatsapp.com/send?phone=5524993081222&text=Oi%20estava%20no%20seu%20Git%20Hub!" target="_blank"><img src="https://img.shields.io/badge/-Whatszap-success" target="_blank"></a>
+  <a href="https://www.instagram.com/fabriciovrsilva" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"></a>
+  <a href = "mailto:fabriciomdasilva8@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
+  <a href="https://www.linkedin.com/in/fabriciomsilva/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a> 
+</div>
 <?php
   include_once("templates/footer.php")
 ?>
